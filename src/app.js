d2ef27df9912193ff07fa22971445c8dd9e88e99
Shruti-369 <alewashruti@gmail.com>
2026-04-21 10:02:18 +0530

    dummyuserimg

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.json()); //middleware to parse JSON data from request body
 
 //push notification API 
 app.post("/signup", async (req, res) => {
-   
+
     //creating a new instance of User model and saving it to the database
     try {
         const user = new User(req.body);
@@ -26,37 +26,66 @@ app.get("/user", async (req, res) => {
 
     try {
         const userEmail = req.body.emailId;
-        const user = await User.find({ emailId : userEmail});
+        const user = await User.find({ emailId: userEmail });
         if (user.length === 0) {
             return res.status(404).json({ message: "User not found" });
         } else {
             res.send(user);
-        }   
+        }
         res.send(user);
     }
     catch (err) {
         res.status(500).json({ error: err.message });
     }
-});        
+});
 
 //feed API - GET /feed - get  all the users from the database
 app.get("/feed", async (req, res) => {
-    try{
+    try {
         const users = await User.find({});
         res.send(users);
     }
-    catch(err){
-        res.status(400).send("Something went wrong");   
+    catch (err) {
+        res.status(400).send("Something went wrong");
     }
-});        
+});
+
+app.delete("/delete", async (req, res) => {
+    const userId = req.body.id;
+    try {
+        //const user = await User.findByIdAndDelete(_id:userId);
+        const user = await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully");
+    } catch (err) {
+        res.status(400).send("Something went wrong");
+    }
+});
+
+app.patch("/update", async (req, res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+    try {
+        const user = await User.findByIdAndUpdate(userId, data, {
+            new: true,
+            runValidators: true
+        });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json({ message: "User updated successfully!", user });
+    }
+    catch (err) {
+        res.status(400).send("UPDATE FAILED: " + err.message);
+    }
+});
 
 connectDB()
     .then(() => {
         console.log("✅ DB connected successfully")
         app.listen(process.env.PORT, () => {
-        console.log(`Server is running on port ${process.env.PORT} 🚀`);
-            });
-        })
+            console.log(`Server is running on port ${process.env.PORT} 🚀`);
+        });
+    })
     .catch((err) => console.log("❌ DB connection failed", err));
 
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,16 +1,21 @@
 const mongoose = require('mongoose');
+const { minify } = require('vite');
 
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
-        required: true
+        required: true,
+        minlength: 4
     },
     lastName: {
         type: String
     },
     emailId: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
     },
     password: {
         type: String,
@@ -18,12 +23,30 @@ const userSchema = new mongoose.Schema({
     },
     age: {
         type: Number,
-        required: true
-    }
+        required: true,
+        min: 18
+    },
+    gender: {
+        type: String,
+        enum: ["male", "female", "other"],
+        lowercase: true,
+        trim: true
+    },
+    photoUrl: {
+        type: String,
+        default: "/assets/dummyUser.webp"
+    },
+    about: {
+        type: String,
+        default: "Hey there! I am using this app."
+    },
+    skills: {
+        type: [String]
+    },
 
 });
 
 const User = mongoose.model("User", userSchema);
 //always capital first letter for model name and it should be singular and mongoose will automatically create a collection with plural name in the database
-  
+
 module.exports = User;
