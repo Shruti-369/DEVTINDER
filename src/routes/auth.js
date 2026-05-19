7ca831c7b1e678bc6866775865c9444ac67f5ab9
Shruti-369 <alewashruti@gmail.com>
2026-05-19 20:16:24 +0530

    cookieUpdate

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -57,8 +57,10 @@ authRouter.post("/login", async (req, res) => {
 
             //add the token to cookie and send response back to the user 
             res.cookie("token", token, {
+                expires: new Date(Date.now() + 8 * 3600000),
                 httpOnly: true,
-                sameSite: "lax"
+                secure: false,
+                sameSite: "None",
             });
             res.status(200)
                 .json({
